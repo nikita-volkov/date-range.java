5c125c9bb9331be78683ab21c62afc6afb556f49
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-10-22 15:34:00 +0300

    Fix the Java version and update the deps

--- src/test/java/com/github/nikita_volkov/java/date_range/Test.java
+++ src/test/java/com/github/nikita_volkov/java/date_range/Test.java
@@ -1,6 +1,6 @@
 package com.github.nikita_volkov.java.date_range;
 
-import com.github.nikita_volkov.java.iterations.fold.LinkedListFold;
+import com.github.nikita_volkov.java.iterations.LinkedListIteration;
 import junit.framework.TestCase;
 
 import java.time.LocalDate;
@@ -10,7 +10,7 @@ public class Test extends TestCase {
 
   public void testIterator() {
     Iterator<LocalDate> iterator = new DateRangeIterator(DateRange.unvalidated(LocalDate.of(2016, 10, 2), LocalDate.of(2016, 10, 7)));
-    LinkedList<LocalDate> list = new LinkedListFold<LocalDate>().consume(iterator);
+    LinkedList<LocalDate> list = new LinkedListIteration<LocalDate>().consume(iterator);
     assertTrue(list.contains(LocalDate.of(2016, 10, 2)));
     assertTrue(list.contains(LocalDate.of(2016, 10, 7)));
     assertTrue(!list.contains(LocalDate.of(2016, 10, 8)));
